758f6caaa8550c1c91b80a17e11b22ae703982ff
totarochristian <totaro.christian.18@gmail.com>
2023-03-15 16:49:17 +0100

    Add function CreateShoppingList that will create a shopping list div with an header and the list inside it.

--- js/script.js
+++ js/script.js
@@ -13,9 +13,28 @@ How can I loop through an array with while loop?
 ---------------------------------------------------------------------------------------------------------------------------
 */
 
-const shoppingList = ["Melanzane","Zucchine","Pere","Spaghetti Barilla","Penne Barilla","Tonno","Aglio","Cipolle"];
+const shoppingList1 = ["Melanzane","Zucchine","Peperoni"];
+const shoppingList2 = ["Pere","Banane","Fragole","Kiwi","Mele"];
+const shoppingList3 = ["Spaghetti","Penne","Fusilli","Conchigliette"];
 
-document.body.appendChild(CreateUnorderedList(shoppingList));
+document.body.appendChild(CreateShoppingList(shoppingList1,"Verdura"));
+document.body.appendChild(CreateShoppingList(shoppingList2,"Frutta"));
+document.body.appendChild(CreateShoppingList(shoppingList3,"Pasta"));
+
+/**
+ * Function that will create a shopping list div with the passed title and passed avlues in a unordered list.
+ * @param {Array} values String arrays (that will be setted as the elements of the list).
+ * @param {string} title Title of the header of the div.
+ * @returns Returns a div with class shoppingList that contains an header and an unordered list.
+ */
+function CreateShoppingList(values,title){
+    const div = document.createElement("div.shoppingList");
+    const header = document.createElement("h2");
+    header.innerText = title;
+    div.appendChild(header);
+    div.appendChild(CreateUnorderedList(values));
+    return div;
+}
 
 /**
  * Function that will create a new unordered list with the elements of the passed array.
